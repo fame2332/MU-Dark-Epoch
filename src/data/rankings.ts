0427dbfb0f5638b1d488d66ac1d5d6f820a93c49
De La Bornoy <rc202300000@gmail.com>
2025-04-20 17:24:46 +0800

    Update rankings.ts

--- src/data/rankings.ts
+++ src/data/rankings.ts
@@ -1,14 +1,9 @@
 import { RankingPlayer } from '../types/types';
 
 export const rankings: RankingPlayer[] = [
-  { rank: 1, name: 'Forgottenone', level: 200 },
-  { rank: 2, name: 'ReidBrad', level: 200 },
-  { rank: 3, name: 'Neggativities', level: 196 },
-  { rank: 4, name: 'White', level: 196 },
-  { rank: 5, name: 'Tipsy', level: 193 },
-  { rank: 6, name: 'Zephyr', level: 191 },
-  { rank: 7, name: 'fcku', level: 188 },
-  { rank: 8, name: 'DARKNIGHT', level: 186 },
-  { rank: 9, name: 'Lucas', level: 186 },
-  { rank: 10, name: 'Marites', level: 185 }
+  { rank: 1, name: 'Forgottenone', level: 211 },
+  { rank: 2, name: 'ReidBrad', level: 210 },
+  { rank: 3, name: 'White', level: 209 },
+  { rank: 4, name: 'Neggativities', level: 206 },
+  { rank: 5, name: 'Zephyr', level: 204 },
 ];
